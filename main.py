472e09b427bb0c335d8300820d76451165816b55
Klay Clarke <klayaclarke@gmail.com>
2021-12-14 13:49:23 -0500

    insert data row into database using cursor

--- main.py
+++ main.py
@@ -12,8 +12,14 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 db = sqlite3.connect('book-collection.db')
 cursor = db.cursor()
-cursor.execute('CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) '
-               'NOT NULL, rating FLOAT NOT NULL)')
+# cursor.execute('CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) '
+#                'NOT NULL, rating FLOAT NOT NULL)')
+
+cursor.execute('INSERT INTO books VALUES(1, "Harry Potter", "J. K. Rowling", "9.3")')
+db.commit()
+
+
+
 
 
 class AddForm(FlaskForm):
