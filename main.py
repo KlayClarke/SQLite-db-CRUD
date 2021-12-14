11e51c1b2075e7e14d583f4647efcf05ea30e8c5
Klay Clarke <klayaclarke@gmail.com>
2021-12-14 12:25:42 -0500

    create form class (allow user to enter their own books into virtual lib using add method - '/add' route)

--- main.py
+++ main.py
@@ -1,20 +1,45 @@
+import os
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+
+
+class AddForm(FlaskForm):
+    book_name = StringField(label='Book Name', validators=[DataRequired()])
+    book_author = StringField(label='Book Author', validators=[DataRequired()])
+    book_rating = StringField(label='Book Rating e.g, 8/10', validators=[DataRequired()])
+    add_book = SubmitField(label='Add Book')
+
 
 all_books = []
 
 
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html')
 
 
-@app.route("/add")
+@app.route("/add", methods=['GET', 'POST'])
 def add():
-    pass
+    book_inventory = all_books
+    form = AddForm()
+    form.validate_on_submit()
+    if request.method == 'POST' and form.validate_on_submit():
+        book_data = {
+            'title': form.book_name.data,
+            'author': form.book_author.data,
+            'rating': form.book_rating.data
+        }
+        all_books.append(book_data)
+        return render_template('index.html', all_books=all_books)
+    return render_template('add.html', form=form)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
